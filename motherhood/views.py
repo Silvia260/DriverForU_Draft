e2ff233d38587450f72151079b7c0be10b06d4ee
Silvia260 <slykaris26@gmail.com>
2022-04-05 21:51:10 +0300

    filter fixed

--- motherhood/views.py
+++ motherhood/views.py
@@ -97,7 +97,7 @@ def search_results(request):
         skill_search = request.GET.get("skill")
         rate_search = request.GET.get("rate")
         filtered_nannies = Nanny.filter_nannies(search_term, skill_search, rate_search).distinct()
-        message=f"{search_term} and {skill_search}  and {rate_search}"
+        message=f"{search_term} and {skill_search}{skill_search}{skill_search} and {rate_search}"
 
         print(filtered_nannies)
 
